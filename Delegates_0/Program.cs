2bfc9b43119d4a318ebabd8b0dbc5b484ee76827
Viktor Speranskyy <vencedor@ukr.net>
2018-10-16 18:05:20 +0300

    Update1

--- Delegates_0/Program.cs
+++ Delegates_0/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Delegates_0
+namespace Delegates0
 {
+    // Создадим делегат
+    delegate int IntOperation(int i, int j);
+
     class Program
     {
-        static void Main(string[] args)
+        // Организуем ряд методов
+        static int Sum(int x, int y)
         {
+            return x + y;
+        }
+
+        static int Prz(int x, int y)
+        {
+            return x * y;
+        }
+
+        static int Del(int x, int y)
+        {
+            return x / y;
+        }
+
+        static void Main()
+        {
+            // Сконструируем делегат
+            IntOperation op1 = new IntOperation(Sum);
+
+            int result = op1(5, 10);
+            Console.WriteLine("Сумма: " + result);
+
+            // Изменим ссылку на метод
+            op1 = new IntOperation(Prz);
+            result = op1(5, 10);
+            Console.WriteLine("Произведение: " + result);
+            Console.ReadLine();
         }
     }
 }
