13fee365a362b5a749dc2da16dbbdb3eefb1e478
Viktor Speranskyy <vencedor@ukr.net>
2018-10-16 18:04:48 +0300

    Add project files.

---
+++ Delegates_0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
