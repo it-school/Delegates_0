10cb7d6d12a2443a6b37c4f79746656a1a63c6dc
it-school <it@osvita.od.ua>
2019-04-03 18:27:05 +0300

    v.2

--- Delegates_0/Program.cs
+++ Delegates_0/Program.cs
@@ -12,12 +12,17 @@ namespace Delegates0
             return x + y;
         }
 
-        static int Prz(int x, int y)
+        static int Min(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int Mul(int x, int y)
         {
             return x * y;
         }
 
-        static int Del(int x, int y)
+        static int Div(int x, int y)
         {
             return x / y;
         }
@@ -31,10 +36,14 @@ namespace Delegates0
             Console.WriteLine("Сумма: " + result);
 
             // Изменим ссылку на метод
-            op1 = new IntOperation(Prz);
+            op1 = new IntOperation(Mul);
             result = op1(5, 10);
             Console.WriteLine("Произведение: " + result);
-            Console.ReadLine();
+
+            // Изменим ссылку на метод
+            op1 = new IntOperation(Min);
+            result = op1(op1(5, 10), 10);
+            Console.WriteLine("Разность: " + result);
         }
     }
 }
